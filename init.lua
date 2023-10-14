823d677acb0d317d9d48738e01c6ad956213ce9f
Rodrigo Meireles de Oliveira <rodrigomeirelesdeoliveira@gmail.com>
2023-10-14 19:44:57 -0300

    Usando autoformat do kickstart

--- init.lua
+++ init.lua
@@ -218,8 +218,8 @@ require('lazy').setup({
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.autoformat',
+  --  require 'kickstart.plugins.debug',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
@@ -582,14 +582,6 @@ cmp.setup {
 
 
 vim.env.python3_host_prog = '/home/rmo/.pyenv/versions/nvim311/bin/python'
-vim.api.nvim_create_autocmd("BufWritePre", {
-  buffer = buffer,
-  callback = function()
-    vim.lsp.buf.format { async = false }
-  end
-})
-
-
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
