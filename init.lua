9cbd2d48288713c5c77805d89098deb72246c716
Rodrigo Meireles <rodrigo.meireles@factored.ai>
2024-07-03 12:15:50 -0300

    Added flash.nvim

--- init.lua
+++ init.lua
@@ -243,6 +243,20 @@ require('lazy').setup({
     },
     lazy = false,
   },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    },
+  },
 
 
 
@@ -507,8 +521,9 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- templ = {},
-  -- pyright = {},
-  rust_analyzer = {},
+  ruff_lsp = {},
+  pyright = {},
+  -- rust_analyzer = {},
   tailwindcss = { filetypes = { 'templ', 'html' } },
   -- tsserver = {},
   -- htmx = { filetypes = { 'html', 'templ' } },
