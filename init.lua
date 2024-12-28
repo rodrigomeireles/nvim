9da36dfeb303f7948ac1db4b2fc65f412b7c859b
Rodrigo Meireles <rodrigo.meireles@factored.ai>
2024-12-28 07:03:33 -0300

    Subbing blink.cmp for nvim-cmp

--- init.lua
+++ init.lua
@@ -99,23 +99,6 @@ require('lazy').setup({
       'folke/neodev.nvim',
     },
   },
-
-  {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
-    },
-  },
-
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim',  opts = {} },
   {
@@ -543,74 +526,10 @@ local servers = {
 require('neodev').setup()
 require('lspconfig').nushell.setup {}
 
--- [[ Configure nvim-cmp ]]
--- See `:help cmp`
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping(function()
-      print("Triggering completion")
-      cmp.mapping.complete {}
-    end, { 'i', 's' }),
-    ['<CR>'] = cmp.mapping.confirm {
-      select = false,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  },
-  sources = {
-    {
-      name = 'nvim_lsp',
-      -- entry_filter = function(entry, context)
-      --   local suggestion_kind = cmp.lsp.CompletionItemKind[entry:get_kind()]
-      --   local node = ts_utils.get_node_at_cursor()
-      --   if node and node:type() == 'argument_list' then
-      --     if suggestion_kind == 'Variable' then
-      --       return true
-      --     else
-      --       return false
-      --     end
-      --   else
-      --     return true
-      --   end
-      -- end
-    },
-    { name = 'luasnip' },
-  },
-}
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
 
 -- Ensure the servers above are installed
 local mason_lspconfig = require 'mason-lspconfig'
@@ -631,7 +550,6 @@ mason_lspconfig.setup_handlers {
     }
   end,
 }
-
 
 vim.env.python3_host_prog = '/home/rmo/.pyenv/versions/nvim311/bin/python'
 vim.bo.tabstop = 4      -- size of a hard tabstop (ts).
